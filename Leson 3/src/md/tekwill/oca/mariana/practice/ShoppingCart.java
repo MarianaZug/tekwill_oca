3f1d3f6ef4e1ae25fc357166ea0b8a52578b88bf
Test <test@MacBook-Test.local>
2018-03-28 20:50:38 +0300

    Leson3

---
+++ Leson 3/src/md/tekwill/oca/mariana/practice/ShoppingCart.java
@@ -0,0 +1,12 @@
+package md.tekwill.oca.mariana.practice;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+
+
+
+
+
+    }
+}
