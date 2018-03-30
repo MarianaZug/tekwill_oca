9483f2aec7bdbe6b7587b7077a6a8c900829bd6d
test <mariana.nicolaev.r@gmail.com>
2018-03-30 20:32:47 +0300

    lesson 4

---
+++ lesson4/src/com/company/ex_2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ex_2 {
+    public static void main(String[] args) {
+        double price = 100.0;
+        double tax = 20.0;
+        int quantity = 5;
+        double total  ;
+        total = price *tax *quantity ;
+        System.out.println(total);
+    }
+}
