2272a22ebc6830a4a26ed98326860148f61d44a2
test <mariana.nicolaev.r@gmail.com>
2018-03-30 19:51:11 +0300

    lesson 4

---
+++ lesson4/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String custName = "Mary Smith", itemdesc = "Shirt" ;
+        String message ;
+        message = custName + " wants to purchase a " + itemdesc ;
+
+        System.out.println(message);
+
+
+	// write your code here
+    }
+}
